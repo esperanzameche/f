91a325717f81eedac03be222f4461c45c7f4cf1c
merce parada <mercedes_balaguera@hotmail.com>
2025-01-07 00:17:56 -0400

    mi primera pagina con html css javascript

---
+++ ficja.js
@@ -0,0 +1,34 @@
+let botonInicio=document.getElementById('boton-navegador1')
+botonInicio.addEventListener('click',inicio)
+let botonAcerca=document.getElementById('boton-navegador2')
+botonAcerca.addEventListener('click',acerca)
+let botonVentajas=document.getElementById('boton-navegador3')
+botonVentajas.addEventListener('click',ventajas)
+let botonServicios=document.getElementById('boton-navegador4')
+botonServicios.addEventListener('click',servicios)
+let botonContactos=document.getElementById('boton-navegador5')
+botonContactos.addEventListener('click',contactos)
+
+function inicio() {
+    alert('inicio')
+}
+function acerca() {
+    alert('acerca')
+}
+function ventajas() {
+    alert('ventajas')
+}
+function servicios() {
+    alert('servicios')
+}
+function contactos() {
+    alert('contactos')
+}
+
+
+
+
+
+
+
+
